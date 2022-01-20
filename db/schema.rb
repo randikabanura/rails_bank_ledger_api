f85062c0cd0f9bc21c2179d9a365fce745adee1a
Banura Randika <randika.banura@gmail.com>
2022-01-20 17:34:06 +0530

    Signup Signin ability add

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 2022_01_20_095210) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.boolean "allow_password_change", default: false
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
